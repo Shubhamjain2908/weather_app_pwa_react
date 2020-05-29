efaaa79acf3c8e3983715c8ca95a295f9d4c0071
Shubham Jain <Shubhamjain2908@gmail.com>
2020-05-30 02:45:18 +0530

    register event listeners

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,1 +1,19 @@
-console.log('Service Wroker loaded');
+const CACHE_NAME = 'version-1';
+const urlsToCache = ['index.html', 'offline.html'];
+
+const self = this;
+
+// Install sw
+self.addEventListener('install', (event) => {
+
+});
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+
+});
+
+// Activate the SW
+self.addEventListener('activate', (event) => {
+
+});
