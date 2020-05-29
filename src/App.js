237432f6d544fe909199eb66f996ceb1b009864b
Shubham Jain <Shubhamjain2908@gmail.com>
2020-05-30 02:00:38 +0530

    adding input & using state to save search query

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import fetchWeather from './api/fetchWeather';
 import './App.css';
 
 
 const App = () => {
+    const [query, setQuery] = useState('');
+
+    const search = async (e) => {
+        if (e.key === 'Enter') {
+            const data = await fetchWeather(query);
+            console.log('Data => ', data);
+        }
+    }
+
     return (
-        <h1>
-            China CKMB!!!
-        </h1>
+        <div class="main-container">
+            <input
+                type="text"
+                className="search"
+                placeholder="Search..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyPress={search}
+            />
+        </div>
     )
 }
 
